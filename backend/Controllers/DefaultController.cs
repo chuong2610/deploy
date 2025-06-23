6b3794207a5d8d75f274004169443429faedb989
chuong2610 <167516421+chuong2610@users.noreply.github.com>
2025-06-23 20:01:34 +0700

    push nginx

---
+++ backend/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("/")] // Đây là root path: http://localhost:5000/
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content("Xin chào", "text/plain");
+        }
+    }
+}
