db858c2fe5d0fedc2c0fdeb706b0e931ec292f2b
chuong2610 <167516421+chuong2610@users.noreply.github.com>
2025-06-26 22:01:23 +0700

    test

--- backend/Controllers/DefaultController.cs
+++ backend/Controllers/DefaultController.cs
@@ -9,7 +9,7 @@ namespace backend.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return Content("Xin chào tất cả ", "text/plain");
+            return Content("Xin chào tất  ", "text/plain");
         }
     }
 }
